9a894a10ef20b6c65837aee55df8de8d5a30d18b
Michael Catanzaro <mike.catanzaro@gmail.com>
2014-05-24 19:21:36 -0500

    Update an old comment header

--- io-interface.hpp
+++ io-interface.hpp
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- *  common.hpp
+ *  io-interface.hpp
  *
  *  Author: Michael Catanzaro <michael.catanzaro@mst.edu>
  *
